dcb7e910ba0f754c8cc793e47e35abee043081b2
Hari <harinarayanan363@gmail.com>
2021-05-05 22:05:41 +0530

    trying my first commit

---
+++ src/main/java/practice/TestGit.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class TestGit {
+
+	@Test
+	public void cool() {
+		System.out.println("learn");
+	}
+}
---
+++ src/test/java/testing/JustGit.java
@@ -0,0 +1,11 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class JustGit {
+
+	@Test
+	public void test(){
+		System.out.println("sample method");
+	}
+}
